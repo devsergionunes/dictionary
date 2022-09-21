53570930ab06c3d0fc404dc9e8b56b6e4dfb02e2
Paulo Sergio <devsergionunes@gmail.com>
2022-09-21 11:20:03 -0300

    config styled-components and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { GlobalStyle } from "./styles/Global";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <h1>Hello Word</h1>
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/styles/indexTheme.ts
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+import ThemeDark from "./themeDark";
+import ThemeLight from "./themeLight";
+
+export function alterTheme(typeTheme = "light") {
+  let theme;
+  if (typeTheme === "light") {
+    theme = createTheme(ThemeLight as any);
+  } else if (typeTheme === "dark") {
+    theme = createTheme(ThemeDark as any);
+  }
+  return theme;
+}
+alterTheme();
